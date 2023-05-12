bcc58e0c109c5611c200066202fbcb21e408007d
tohilla <40602617+tohilla@users.noreply.github.com>
2023-05-12 22:07:52 +0100

    Add files via upload

---
+++ Data Pipeline/DataExtractor.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+public class DataExtractor
+{
+    private readonly string sourceConnectionString;
+
+    public DataExtractor(string sourceConnectionString)
+    {
+        this.sourceConnectionString = sourceConnectionString;
+    }
+
+    public Dictionary<string, DataTable> ExtractDataFromTables(List<string> tablesToExtract)
+    {
+        Dictionary<string, DataTable> extractedData = new Dictionary<string, DataTable>();
+
+        using (SqlConnection connection = new SqlConnection(sourceConnectionString))
+        {
+            connection.Open();
+
+            foreach (string tableName in tablesToExtract)
+            {
+                DataTable dataTable = ExtractDataFromTable(sourceConnectionString, tableName);
+                extractedData.Add(tableName, dataTable);
+            }
+
+            connection.Close();
+        }
+
+        return extractedData;
+    }
+
+    public DataTable ExtractDataFromTable(string connectionString, string tableName)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = $"SELECT * FROM {tableName}";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+}
---
+++ Data Pipeline/DataInserter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+public class DataInserter
+{
+    private readonly string destinationConnectionString;
+
+    public DataInserter(string destinationConnectionString)
+    {
+        this.destinationConnectionString = destinationConnectionString;
+    }
+
+    public void InsertDataIntoTables(Dictionary<string, DataTable> extractedData)
+    {
+        using (SqlConnection connection = new SqlConnection(destinationConnectionString))
+        {
+            connection.Open();
+
+            foreach (var entry in extractedData)
+            {
+                string tableName = entry.Key;
+                DataTable data = entry.Value;
+
+                CreateDestinationTable(connection, tableName, data);
+                BulkInsertData(connection, tableName, data);
+
+                Console.WriteLine($"Data inserted into the destination table for table: {tableName}");
+            }
+
+            connection.Close();
+        }
+    }
+
+    private void CreateDestinationTable(SqlConnection connection, string tableName, DataTable data)
+    {
+        // Use SqlBulkCopy to transfer data from the source table to the destination table
+        using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+        {
+            bulkCopy.DestinationTableName = tableName;
+            bulkCopy.WriteToServer(data);
+        }
+    }
+
+
+    private void BulkInsertData(SqlConnection connection, string tableName, DataTable data)
+    {
+        using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+        {
+            bulkCopy.DestinationTableName = tableName;
+            bulkCopy.WriteToServer(data);
+        }
+    }
+
+    private string GetSqlType(Type dataType)
+    {
+        if (dataType == typeof(int))
+            return "INT";
+        else if (dataType == typeof(string))
+            return "NVARCHAR(MAX)";
+        else if (dataType == typeof(DateTime))
+            return "DATETIME";
+        // Add more data type mappings as needed
+
+        return "NVARCHAR(MAX)";
+    }
+
+}
